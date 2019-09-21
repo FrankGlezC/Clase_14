d6a7777971eb7452d7bdac39ea212d52c33c49ab
unknown <francisco.fjgc49@gmail.com>
2019-09-20 20:33:03 -0500

    index.js

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const app = express()
 const port=3000
+const handleRoute = function(req,res)
+{
+res.send('Hello World!')
+}
 
+/*
 app.get('/',function(req,res)
 {
 res.send('Hello World!')
 })
+*/
+
+app.get('/',handleRoute)
+
+app.get('/another-route',function(req,res)
+{
+res.send('Hello to another route')
+})
 
 app.listen(port,function()
-{console.log('Example app listening on port ${port}!')})
+{console.log(`Example app listening on port ${port}!`)})
